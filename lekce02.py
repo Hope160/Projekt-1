d69b34b7196cf4098bc446e55f73d5dfe57cf631
Hope160 <syblikovaha@seznam.cz>
2024-12-07 22:35:31 +0100

    Další přenos

--- lekce02.py
+++ lekce02.py
@@ -26,3 +26,20 @@ print('Na posledním indexu je:',muj_seznam_1[7])
 print('V intervalu od 2 do 5 je:',muj_seznam_1[2:6])
 print('Každý třetí člen je:',muj_seznam_1[::3])
 
+jmeno_01 = 'Martin'
+vaha = 80
+vyska  = 2
+BMI = vaha / vyska ** 2
+
+if BMI > 40:
+    kategorie = 'těžká obezita'
+elif BMI > 30:
+    kategorie = 'obezita'
+elif BMI > 25:
+    kategorie = 'mírná nadváha'
+elif BMI > 18.5:
+    kategorie = 'zdravá váha'
+else:
+    kategorie = 'podvýživa'
+
+print(jmeno_01,"tvé BMI je", BMI,",což spadá do kategorie",kategorie, ".")
