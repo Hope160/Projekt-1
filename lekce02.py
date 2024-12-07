6ee9d5e5622465284794a8bd4808d7cf3b64285b
Hope160 <syblikovaha@seznam.cz>
2024-12-08 00:31:57 +0100

    Poslední cvičení z lekce 2

--- lekce02.py
+++ lekce02.py
@@ -56,3 +56,21 @@ print('Nová jména přidána: ', zamestnanci_a)
 zamestnanci_b = zamestnanci.copy()
 zamestnanci_b.insert(1, 'Bruno')
 print('Nová jména vložená:',zamestnanci_b)
+
+vstupni_cisla = [1, 2, 3, 4, 5, 6, 7]
+vstupni_pismena = ["p", "ú", "s", "č", "p", "s", "n"]
+tyden = ('pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota', 'neděle')
+
+cislo_dne = 5
+if cislo_dne in vstupni_cisla:
+    print("Správná vstupní hodnota!")
+    den_tydne = tyden[cislo_dne - 1]
+    if den_tydne.startswith(vstupni_pismena[cislo_dne - 1]):
+        print("Správné písmeno")
+    else:
+        print("Špatné písmeno!")
+
+else:
+    print("Špatná vstupní hodnota!")
+
+
